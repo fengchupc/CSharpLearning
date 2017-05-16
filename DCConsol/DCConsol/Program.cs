4a87a3011618d9133ec5f14f30e3e173dd3066c7
fengchupc <fengchupc@Hotmail.com>
2017-05-16 11:03:19 +1000

    Add Console application for Data Calculation

---
+++ DCConsol/DCConsol/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCConsol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculateDate();
+        }
+
+        public static void calculateDate()
+        {
+            DateCalculation dc = new DateCalculation();
+
+            Console.Write("Enter the start date (yyyy-mm-dd): ");
+            String a = Console.ReadLine();
+            dc.startDate = Convert.ToDateTime(a);
+            Console.Write("Enter the end date (yyyy-mm-dd): ");
+            a = Console.ReadLine();
+            dc.endDate = Convert.ToDateTime(a);
+
+            long ticks = dc.endDate.Ticks - dc.startDate.Ticks;
+
+            TimeSpan ts = new TimeSpan(ticks);
+             
+            DateTime dt = new DateTime(ticks);
+
+            Console.Write("Difference between these two dates are: {0} years, {1} months, {2} days,", ts.Days/365, ts.Days%365/30, ts.Days%365%30);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
